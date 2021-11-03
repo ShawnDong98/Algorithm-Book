a929a922cced8e7c639f6cc37691c27f4ac18cbd
ShawnDong98 <ShawnDong98@gmail.com>
2021-11-03 10:02:24 +0800

    leetcode 55

--- leetcode/python/55.py
+++ leetcode/python/55.py
@@ -14,4 +14,15 @@ class Solution:
                 max_i = i + jump
 
         return max_i >= i
+
+    def canJump_solution2(self, nums: List[int]) -> bool:
+        # 初始化当前能到达的最远的位置
+        farthest = 0
+        # i 为当前位置， jump是当前位置的跳数
+        for i, jump in enumerate(nums):
+            # 如果当前位置小于最远的位置，并且当前位置+跳数>最远位置
+            if i  <= farthest:
+                farthest = max(farthest, i + jump)
+
+        return farthest >= i
              
