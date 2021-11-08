f44a16cf090a26092a255e6418e4b5639b355219
ShawnDong98 <ShawnDong98@gmail.com>
2021-11-08 13:05:34 +0800

    leetcode 134

---
+++ leetcode/python/134.py
@@ -0,0 +1,23 @@
+from typing import List
+
+class Solution:
+    """
+        如果总油量减去总消耗大于等于零那么一定可以跑完一圈，说明 各个站点的加油站 剩油量rest[i]相加一定是大于等于零的。
+
+        每个加油站的剩余量rest[i]为gas[i] - cost[i]。
+
+        i从0开始累加rest[i]，和记为curSum，一旦curSum小于零，说明[0, i]区间都不能作为起始位置，起始位置从i+1算起，再从0计算curSum。
+
+    """
+    def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
+        start = 0
+        curSum = 0
+        totalSum = 0
+        for i in range(len(gas)):
+            curSum += gas[i] - cost[i]
+            totalSum += gas[i] - cost[i]
+            if curSum < 0:
+                start = i + 1
+                curSum = 0
+        if totalSum < 0: return -1
+        return start   
