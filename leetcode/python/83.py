c48e47d71570bdeb2f50235c2c0bff199ea5e086
ShawnDong98 <ShawnDong98@gmail.com>
2021-11-29 11:07:10 +0800

    leetcode 83.py

---
+++ leetcode/python/83.py
@@ -0,0 +1,13 @@
+class Solution:
+    def deleteDuplicates(self, head: ListNode) -> ListNode:
+        if not head:
+            return head
+        dummy = ListNode(0)
+        dummy.next = head
+        cur = head
+        while cur:
+            while cur.next and cur.val == cur.next.val:
+                cur.next = cur.next.next
+            cur = cur.next
+
+        return dummy.next
