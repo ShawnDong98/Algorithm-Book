13b332b0995bb368b84f16f1bae145c049949e0b
ShawnDong98 <ShawnDong98@gmail.com>
2021-11-30 13:43:56 +0800

    leetcode 61.py

---
+++ leetcode/python/61.py
@@ -0,0 +1,31 @@
+from typing import Optional
+class Solution:
+    """
+        - k %= n
+        - first 指针从头往后走 k 步
+        - second 和 first 同时往后走， 当 first 走到最后一个节点时，结束
+
+    """
+    def rotateRight(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
+        if not head: return None
+        n = 0
+        p = head
+        while p:
+            p = p.next
+            n += 1
+        k %= n
+        first = head
+        second = head
+        while k:
+            first = first.next
+            k -= 1
+        while(first.next):
+            first = first.next
+            second = second.next
+        first.next = head
+        head = second.next
+        second.next = None
+
+        return head
+
+
