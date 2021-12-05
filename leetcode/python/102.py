57fa0a5fa2bcf007afb1f9284c80cd65020dee61
ShawnDong98 <ShawnDong98@gmail.com>
2021-12-05 11:13:15 +0800

    leetcode 102.py

---
+++ leetcode/python/102.py
@@ -0,0 +1,17 @@
+from typing import List
+
+class Solution:
+    def levelOrder(self, root: TreeNode) -> List[List[int]]:
+        if not root: return []
+        queue = [root]
+        res = []
+        while queue:
+            level = []
+            for _ in range(len(queue)):
+                node = queue.pop(0)
+                level.append(node.val)
+                if node.left: queue.append(node.left)
+                if node.right: queue.append(node.right)
+            res.append(level)
+        return res
+
