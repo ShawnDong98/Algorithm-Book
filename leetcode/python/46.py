5f2b7067f7583717ceb88baa0c3d94ff0a39a1bf
ShawnDong98 <ShawnDong98@gmail.com>
2021-12-14 17:21:54 +0800

    leetcode 46.py add backtrack

--- leetcode/python/46.py
+++ leetcode/python/46.py
@@ -1,5 +1,39 @@
+from typing import List
 from itertools import permutations
 
 class Solution:
+    """
+    1 2 3
+
+    """
     def permute(self, nums: List[int]) -> List[List[int]]:
+        def dfs(nums, size, depth, path, used, res):
+            if depth == size:
+                res.append(path.copy())
+                return
+
+            for i in range(size):
+                if not used[i]:
+                    used[i] = True
+                    path.append(nums[i])
+
+                    dfs(nums, size, depth + 1, path, used, res)
+
+                    used[i] = False
+                    path.pop()
+        size = len(nums)
+        if len(nums) == 0:
+            return []
+
+        used = [False] * size
+        res = []
+        dfs(nums, size, 0, [], used, res)
+        return res
+
+    def permute_itertools(self, nums: List[int]) -> List[List[int]]:
         return list(permutations(nums))
+
+
+nums = [1,2,3]
+S = Solution()
+print(S.permute(nums))
