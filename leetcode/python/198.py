a1000898039d351cfe92a6766a1c37023ce49086
ShawnD <ShawnDong98@gmail.com>
2021-12-27 14:06:30 +0800

    leetcode 198.py

--- leetcode/python/198.py
+++ leetcode/python/198.py
@@ -19,8 +19,7 @@ class Solution:
 
         dp = [0] * n
         dp[0], dp[1] = nums[0], nums[1]
-
         for i in range(2, n):
-            dp[i] = max(dp[i - 2], dp[i - 3] if i >= 3 else 0) + nums[i]
+            dp[i] = max(dp[i-2], dp[i-3] if i >= 3 else 0) + nums[i]
 
-        return max(dp[-1], dp[-2])
+        return max(dp)
