504b4bf1a2f9a1eb727da8279313530e51fdbf78
ShawnD <ShawnDong98@gmail.com>
2021-12-27 14:16:03 +0800

    leetcode 300.py

--- leetcode/python/300.py
+++ leetcode/python/300.py
@@ -16,13 +16,13 @@ class Solution:
 
     """
     def lengthOfLIS(self, nums: List[int]) -> int:
-        if not nums: return 0
+        if len(nums) == 0: return 0
         dp = [1] * len(nums)
         for i in range(len(nums)):
             for j in range(i):
-                # 如果要求非严格递增，将此行 '<' 改为 '<=' 即可。
+                # 如果非严格递增，将 < 改为 <=
                 if nums[j] < nums[i]:
                     dp[i] = max(dp[i], dp[j] + 1)
 
         return max(dp)
-            
+
