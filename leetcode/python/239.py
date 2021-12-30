025004e16c8022ace8ca716778b29a1168b6802a
ShawnD <ShawnDong98@gmail.com>
2021-12-30 13:33:15 +0800

    leetcode 299.py

---
+++ leetcode/python/239.py
@@ -0,0 +1,26 @@
+from typing import List
+from collections import deque
+
+class Solution:
+    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+        q, ret = deque(), []
+        for i, j in enumerate(nums):
+            while q and nums[q[-1]] < j:
+                q.pop()
+            if q and q[0] <= i - k:
+                q.popleft()
+            q.append(i)
+            if i >= k - 1:
+                ret.append(nums[q[0]])
+        return ret
+
+    def maxSlidingWindow_tle(self, nums: List[int], k: int) -> List[int]:
+        if len(nums) <= k: return [max(nums)]
+        left = 0
+        right = left + k
+        res = []
+        while right <= len(nums):
+            res.append(max(nums[left:right]))
+            left += 1
+            right += 1
+        return res
