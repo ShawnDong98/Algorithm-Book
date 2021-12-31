87e10ded3a12d95d5105ad6441c6c417fcbfe37f
ShawnD <ShawnDong98@gmail.com>
2021-12-31 12:10:15 +0800

    acwing 788.py

---
+++ acwing/788.py
@@ -0,0 +1,41 @@
+def merge_sort(arr, l, r, tmp):
+    if l >= r: return 0
+    mid = (l + r) // 2
+    res = merge_sort(arr, l, mid, tmp) + merge_sort(arr, mid + 1, r, tmp)
+
+    k = 0
+    i = l
+    j = mid + 1
+    while i <= mid and j <= r:
+        if (arr[i] <= arr[j]): 
+            tmp[k] = arr[i]
+            k += 1
+            i += 1
+        else:
+            res += mid - i + 1
+            tmp[k] = arr[j]
+            k += 1
+            j += 1
+    while i <= mid:
+        tmp[k] = arr[i]
+        k += 1
+        i += 1
+    while j <= r:
+        tmp[k] = arr[j]
+        k += 1
+        j += 1
+    
+    i = l
+    j = 0
+    while i <= r:
+        arr[i] = tmp[j]
+        i += 1
+        j += 1
+    
+    return res
+
+
+n = int(input())
+arr = list(map(int, input().split()))
+tmp = [0] * n
+print(merge_sort(arr, 0, n - 1, tmp))
