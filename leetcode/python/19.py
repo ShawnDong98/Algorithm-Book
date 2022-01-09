fab9dbe7052b0bbd6f0893dc73efff2f8f909de6
ShawnD <ShawnDong98@gmail.com>
2022-01-09 21:21:43 +0800

    leetcode 19.py

--- leetcode/python/19.py
+++ leetcode/python/19.py
@@ -13,8 +13,10 @@ class Solution:
         second = dummy
         for i in range(n + 1):
             first = first.next
+
         while first:
             first = first.next
             second = second.next
         second.next = second.next.next
         return dummy.next
+
