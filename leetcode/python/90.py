d277295f32704c9cfd6e91c69ea233528e7c94d0
ShawnD <ShawnDong98@gmail.com>
2022-01-25 12:10:45 +0800

    leetcode 90.py

--- leetcode/python/90.py
+++ leetcode/python/90.py
@@ -1,6 +1,24 @@
 from typing import List
 from itertools import combinations
 class Solution:
+    def subsetsWithDup_v20220125(self, nums: List[int]) -> List[List[int]]:
+        def dfs(nums, u):
+            if u == len(nums):
+                res.append(path.copy())
+                return
+            k = u + 1
+            while k < len(nums) and nums[k] == nums[u]: k += 1
+            for i in range(k-u+1):
+                dfs(nums, k)
+                path.append(nums[u])
+            for i in range(k-u+1): path.pop()
+
+        res = []
+        path = []
+        nums = sorted(nums)
+        dfs(nums, 0)
+        return res
+
     def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
         def dfs(nums, index, res, path):
             res.append(path.copy())
