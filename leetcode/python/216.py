19ab6bf1f7d7f5c455a85cbed247d6747b4da6a7
ShawnD <ShawnDong98@gmail.com>
2022-01-30 00:51:11 +0800

    leetcode 216.py

--- leetcode/python/216.py
+++ leetcode/python/216.py
@@ -1,11 +1,25 @@
 from typing import List
 class Solution:
-    """
-    依次枚举每个数从哪个位置上选
-
-    dfs(枚举到了第几个数字， 开始枚举的位置， 当前选择的所有数的和)
-    """
+    def combinationSum3_v20220130(self, k: int, n: int) -> List[List[int]]:
+        def dfs(start, n, k):
+            if not n:
+                if not k: res.append(path.copy())
+            elif k:
+                for i in range(start, 10):
+                    if n >= i:
+                        path.append(i)
+                        dfs(i + 1, n - i, k - 1)
+                        path.pop()
+        res = []
+        path = []
+        dfs(1, n, k)
+        return res
     def combinationSum3(self, k: int, n: int) -> List[List[int]]:
+        """
+        依次枚举每个数从哪个位置上选
+
+        dfs(枚举到了第几个数字， 开始枚举的位置， 当前选择的所有数的和)
+        """
         path = []
         used = [False] * 10
         res = []
