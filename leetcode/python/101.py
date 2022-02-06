60f83bcc254c0e1b400c5e0aed975696e98f47a7
ShawnD <ShawnDong98@gmail.com>
2022-02-06 18:44:35 +0800

    leetcode 101.py

--- leetcode/python/101.py
+++ leetcode/python/101.py
@@ -1,5 +1,12 @@
 class Solution:
     def isSymmetric(self, root: TreeNode) -> bool:
+        def dfs(p, q):
+            if not p and not q: return True
+            if not p or not q or p.val != q.val: return False
+            return dfs(p.left, q.right) and dfs(p.right, q.left)
+        if not root: return True
+        return dfs(root.left, root.right)
+    def isSymmetric(self, root: TreeNode) -> bool:
         def dfs(left, right):
             if not left and not right: return True
             elif not left or not right: return False
