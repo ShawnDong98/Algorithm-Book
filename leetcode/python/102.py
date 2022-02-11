bae20fee3cd9ab06fe211e5e5b786425b4d94dee
ShawnD <ShawnDong98@gmail.com>
2022-02-11 15:42:42 +0800

    leetcode 102.py

--- leetcode/python/102.py
+++ leetcode/python/102.py
@@ -1,6 +1,23 @@
 from typing import List
-
+import collections
 class Solution:
+    def levelOrder_v20220211(self, root: TreeNode) -> List[List[int]]:
+        res = []
+        q = collections.deque([])
+        if root is not None: q.append(root)
+        while q:
+            level = []
+            l = len(q)
+
+            while l:
+                t = q.popleft()
+                level.append(t.val)
+                if t.left: q.append(t.left)
+                if t.right: q.append(t.right)
+                l -= 1
+
+            res.append(level)
+        return res
     def levelOrder(self, root: TreeNode) -> List[List[int]]:
         if not root: return []
         queue = [root]
