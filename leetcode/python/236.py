6af8a4627f59f77fca352f679359c494ce5fe69d
ShawnD <ShawnDong98@gmail.com>
2022-02-13 13:20:16 +0800

    leetcode 236.py

--- leetcode/python/236.py
+++ leetcode/python/236.py
@@ -1,11 +1,24 @@
 class Solution:
-    """
-        - 如果以root为根节点的树中，p和q都在树中，则root就是最近公共祖先
-        - 如果只包含p， 则返回p
-        - 如果只包含q， 则返回q
-        - 如果都不包含，则返回None
-    """
+    def lowestCommonAncestor_v20220213(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        def dfs(root, p, q):
+            nonlocal res
+            if not root: return 0
+            state = dfs(root.left, p, q)
+            if root == p: state |= 1
+            elif root == q: state |= 2
+            state |= dfs(root.right, p, q)
+            if state == 3 and not res: res = root
+            return state
+        res = None
+        dfs(root, p, q)
+        return res
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        """
+            - 如果以root为根节点的树中，p和q都在树中，则root就是最近公共祖先
+            - 如果只包含p， 则返回p
+            - 如果只包含q， 则返回q
+            - 如果都不包含，则返回None
+        """
         if(root is None or root == p or root == q):
             return root
 
