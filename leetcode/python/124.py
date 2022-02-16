8c232df0251a74d8e50b996ab8105621ea7e6e5c
ShawnD <ShawnDong98@gmail.com>
2022-02-16 12:25:52 +0800

    leetcode 124.py

--- leetcode/python/124.py
+++ leetcode/python/124.py
@@ -1,6 +1,17 @@
 from typing import Optional
 
 class Solution:
+    def maxPathSum_v20220216(self, root: Optional[TreeNode]) -> int:
+        def dfs(root):
+            nonlocal res
+            if not root: return 0
+            left = dfs(root.left)
+            right = dfs(root.right)
+            res = max(res, left + right + root.val)
+            return max(0, max(left, right) + root.val)
+        res = float('-inf')
+        dfs(root)
+        return res
     def maxPathSum(self, root: Optional[TreeNode]) -> int:
         self.maxsum = float('-inf')
         def dfs(root):
