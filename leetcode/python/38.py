f739c6b78283af32686aef59d0b81d18f57963c9
ShawnD <ShawnDong98@gmail.com>
2022-02-22 22:34:00 +0800

    leetcode 38.py

--- leetcode/python/38.py
+++ leetcode/python/38.py
@@ -1,10 +1,24 @@
 class Solution:
+    def countAndSay_v20220222(self, n: int) -> str:
+        s = "1"
+        t = ""
+        for i in range(n-1):
+            j = 0
+            while j < len(s):
+                k = j + 1
+                while k < len(s) and s[k] == s[j]: k += 1
+                t += str(k - j) + s[j]
+                j = k
+            s = t
+        return s
+
+    @lru_cache(None)
+    def countAndSay(self, n: int) -> str:
     """
         将这个看成两个题， 如何将形如"1211"转换成"111221"
     """
-    @lru_cache(None)
-    def countAndSay(self, n: int) -> str:
         def count(s):
+
             ans, cnt = [], 0
             for i, c in enumerate(s + "#"):
                 if not i or s[i-1] == c:
@@ -14,5 +28,5 @@ class Solution:
                     ans.append(s[i-1])
                     cnt = 1
             return "".join(ans)
-    	return "1" if n == 1 else count(countAndSay(n-1))
+        return "1" if n == 1 else count(countAndSay(n-1))
 
