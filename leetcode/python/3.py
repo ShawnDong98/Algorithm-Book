2f32228a11fff8d95576e692f38be5b18a143549
ShawnD <ShawnDong98@gmail.com>
2022-03-06 17:43:14 +0800

    leetcode 3.py

--- leetcode/python/3.py
+++ leetcode/python/3.py
@@ -1,8 +1,22 @@
+from collections import defaultdict
+
 class Solution:
+    def lengthOfLongestSubstring_v20220306(self, s: str) -> int:
+        res = 0
+        heap = defaultdict(int)
+        i, j = 0, 0
+        while i < len(s):
+            heap[s[i]] += 1
+            while heap[s[i]] > 1:
+                heap[s[j]] -= 1
+                j += 1
+            res = max(res, i - j + 1)
+            i += 1
+        return res
+    def lengthOfLongestSubstring(self, s: str) -> int:
     """
     分别以每个字符为起始字符， 逐个字符判断， 直到在哈希表中发现重复字符
     """
-    def lengthOfLongestSubstring(self, s: str) -> int:
         ans = 0
         hash_set = set()
 
