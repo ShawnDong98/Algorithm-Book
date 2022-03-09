1e714410253d3b211794d2e3dd6f39499413ce84
ShawnD <ShawnDong98@gmail.com>
2022-03-09 23:11:00 +0800

    leetcode 1312.py

---
+++ leetcode/python/131.py
@@ -0,0 +1,22 @@
+class Solution:
+    def partition(self, s: str) -> List[List[str]]:
+        def dfs(s, u):
+            if u == len(s): res.append(path.copy())
+            else:
+                for i in range(u, len(s)):
+                    if st[u][i]:
+                        path.append(s[u:i+1])
+                        dfs(s, i + 1)
+                        path.pop()
+        res = []
+        path = []
+        st = [[False] * len(s) for _ in range(len(s))]
+        for j in range(len(s)):
+            for i in range(0, j+1):
+                if i == j: st[i][j] = True
+                elif s[i] == s[j]:
+                    if i + 1 > j - 1 or st[i+1][j-1]:
+                        st[i][j] = True
+        dfs(s, 0)
+        return res
+
