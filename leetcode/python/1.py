25f1bb575b2682d87bb55e756d96890b83629d79
ShawnD <ShawnDong98@gmail.com>
2022-03-17 22:40:41 +0800

    leetcode 1.py

--- leetcode/python/1.py
+++ leetcode/python/1.py
@@ -1,6 +1,16 @@
+from collections import defaultdict
 from typing import List
 
 class Solution:
+    def twoSum_v20220317(self, nums: List[int], target: int) -> List[int]:
+        hash_map = defaultdict(int)
+        res = []
+        for i in range(len(nums)):
+            if target - nums[i] in hash_map:
+                res.append(hash_map[target - nums[i]])
+                res.append(i)
+                return res
+            hash_map[nums[i]] = i
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         for i in range(len(nums)):
             for j in range(i+1, len(nums)):
