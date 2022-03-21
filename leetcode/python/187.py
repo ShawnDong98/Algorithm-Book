8749ebfed9de6989d9f0b6a02b36ddfa76cfae5e
ShawnD <ShawnDong98@gmail.com>
2022-03-21 17:15:32 +0800

    leetcode 187.py

--- leetcode/python/187.py
+++ leetcode/python/187.py
@@ -2,10 +2,11 @@ from collections import defaultdict
 from typing import List
 class Solution:
     def findRepeatedDnaSequences(self, s: str) -> List[str]:
+        res = []
         hash = defaultdict(int)
         for i in range(len(s)-9):
             hash[s[i:i+10]] += 1
-        res = []
+
         for k, v in hash.items():
             if v > 1:
                 res.append(k)
