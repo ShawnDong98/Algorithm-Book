e3efe7a6fcd99784fb931704e56f70dae9708c7c
ShawnD <ShawnDong98@gmail.com>
2022-04-03 10:48:32 +0800

    leetcode 69.py

--- leetcode/python/69.py
+++ leetcode/python/69.py
@@ -1,17 +1,15 @@
 class Solution:
     def mySqrt(self, x: int) -> int:
-        if x <= 1:
-            return x
+        if x <= 1: return x
         low, high = 1, x
         while low <= high:
             mid = (low + high) // 2
-            if  mid < x / mid:
-                low = mid + 1
-            elif mid > x / mid:
+            if mid * mid > x:
                 high = mid - 1
+            elif mid * mid < x:
+                low = mid + 1
             else:
                 return mid
         return high
-
 
 
