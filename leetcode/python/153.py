56733b2a224c71aeed6a9a20ea436ffef634561a
ShawnD <ShawnDong98@gmail.com>
2022-04-07 13:45:06 +0800

    leetcode 153.py

--- leetcode/python/153.py
+++ leetcode/python/153.py
@@ -1,5 +1,16 @@
 from typing import List
 
 class Solution:
+    def findMin_v20220407(self, nums: List[int]) -> int:
+        l = 0
+        r = len(nums) - 1
+        if nums[r] >= nums[l]: return nums[0]
+        while l < r:
+            mid = (l + r) >> 1
+            if nums[mid] < nums[0]:
+                r = mid
+            else:
+                l = mid + 1
+        return nums[r]
     def findMin(self, nums: List[int]) -> int:
-        return min(nums.sort())
+        return min(nums)
