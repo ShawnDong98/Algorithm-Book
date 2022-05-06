2cb34e6b77e45a0dd97dea87f6c6f5913194259a
ShawnD <ShawnDong98@gmail.com>
2022-05-06 16:35:31 +0800

    acwing 787.py

--- acwing/787.py
+++ acwing/787.py
@@ -1,12 +1,17 @@
-def merge_sort(arr, l, r, tmp):
-    if l >= r: return
-    mid = (l + r) // 2
-    merge_sort(arr, l, mid, tmp)
-    merge_sort(arr, mid + 1, r, tmp)
-    i = l
+"""
+1. 确定分界点： mid = (l + r) / 2
+2. 递归排序 left, right
+3. 归并—— 合二为一
+"""
+def merge_sort(arr, left, right, tmp):
+    if left >= right: return
+    mid = left + right >> 1
+    merge_sort(arr, left, mid, tmp)
+    merge_sort(arr, mid + 1, right, tmp)
+    i = left
     j = mid + 1
     k = 0
-    while (i <= mid and j <= r):
+    while i <= mid and j <= right:
         if arr[i] <= arr[j]:
             tmp[k] = arr[i]
             i += 1
@@ -18,12 +23,12 @@ def merge_sort(arr, l, r, tmp):
         tmp[k] = arr[i]
         i += 1
         k += 1
-    while j <= r:
+    while j <= right:
         tmp[k] = arr[j]
         j += 1
         k += 1
-    i, j = l, 0
-    while i <= r:
+    i, j = left, 0
+    while i <= right:
         arr[i] = tmp[j]
         i += 1
         j += 1
@@ -31,9 +36,5 @@ def merge_sort(arr, l, r, tmp):
 n = int(input())
 arr = list(map(int, input().split()))
 tmp = [0] * n
-merge_sort(arr, 0, n - 1, tmp)
+merge_sort(arr, 0, n-1, tmp)
 print(' '.join(map(str, arr)))
-
-
-
-
