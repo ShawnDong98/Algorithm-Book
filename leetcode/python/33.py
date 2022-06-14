0b2fb4a5d41f1495827183112d6859dac8c77484
ShawnD <ShawnDong98@gmail.com>
2022-06-14 22:06:51 +0800

    33.py

--- leetcode/python/33.py
+++ leetcode/python/33.py
@@ -2,10 +2,28 @@ from typing import List
 
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        try:
-            return nums.index(target)
-        except:
-            return -1
+        if len(nums) == 0: return -1
+        l = 0
+        r = len(nums) - 1
+        while l < r:
+            mid = l + r + 1 >> 1
+            if nums[mid] >= nums[0]: l = mid
+            else: r = mid - 1
+
+
+        if target >= nums[0]: l = 0
+        else:
+            l = r + 1
+            r = len(nums) - 1
+
+        while l < r:
+            mid = l + r >> 1
+            if nums[mid] >= target: r = mid
+            else: l = mid + 1
+
+        if nums[r] == target: return r
+        return -1
+
 
 
 nums = [4,5,6,7,0,1,2]
